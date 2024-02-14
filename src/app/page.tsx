b709479bbf68b92a99fae727ac4ed364443da213
Hoàng Phạm <hoang.minh.pham1510@gmail.com>
2024-02-14 11:20:09 -0600

    Added moving button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import useWindowSize from "@/actions/windowSize";
 import { GreenButton } from "@/components/GreenButton";
 import { useState } from "react";
-
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
 export default function Home() {
   const [count, setCount] = useState(0);
   const beglist = [
@@ -22,7 +25,14 @@ export default function Home() {
     "wouldn't you reconsider?",
     "is that your final answer?",
     "your breaking my heart :(",
+    "catch me if you can >:(",
   ];
+
+  //TODO: get the dynamic width of element to plug in this
+  const style = {
+    top: `${getRandomInt((useWindowSize()?.height ?? 40) - 40)}px`,
+    left: `${getRandomInt((useWindowSize()?.width ?? 203.27) - 203.27)}px`,
+  };
 
   return (
     <div className="flex flex-col justify-center items-center	">
@@ -30,11 +40,21 @@ export default function Home() {
         <img className="pt-4" src="\hehehehe.png" alt="cute meme" />
       </div>
       <div className="flex flex-row gap-4">
-        <GreenButton size={count}/>
+        <GreenButton
+          size={count < beglist.length - 1 ? count : beglist.length - 1}
+        />
         <button
-          className="bg-red-500 hover:bg-red-700  text-white font-bold py-2 px-4 rounded"
+          id="RedButton"
+          className={
+            (count < beglist.length - 1 ? "flex " : "absolute ") +
+            " self-center bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded transition-all ease-in-out"
+          }
           type="button"
           onClick={() => setCount(count + 1)}
+          onMouseOver={
+            count < beglist.length - 1 ? () => {} : () => setCount(count + 1)
+          }
+          style={count < beglist.length - 1 ? {} : style}
         >
           {beglist[count < beglist.length - 1 ? count : beglist.length - 1]}
         </button>
--- src/components/GreenButton.tsx
+++ src/components/GreenButton.tsx
@@ -1,17 +1,19 @@
-"use client"
+"use client";
 
-import Link from 'next/link'
+import Link from "next/link";
 
 export function GreenButton(props: { size: number }) {
   console.log(props.size);
-  
+  const fontScale = 1 + props.size;
   return (
     <Link
       className={
-        "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-        + ` size-${props.size}`
+        "flex flex-col justify-center py-2 px-4 content-center bg-green-500 hover:bg-green-700 text-white font-bold rounded"
       }
-      href={'/yay'}
+      style={{
+        fontSize: `${fontScale}em`,
+      }}
+      href={"/yay"}
     >
       YES
     </Link>
